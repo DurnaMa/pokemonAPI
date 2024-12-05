7b378bd086c5745da0112843ea3382c1c7cffc1c
Mahir Durna <159443825+DurnaMa@users.noreply.github.com>
2024-12-05 12:26:19 +0100

    card

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 let fetchPokemons = [];
+let evolutionChains = [];
 
 function init() {
     fetchPokemon().then(() => pokemonCard());
+    evolutionChain();
+    evolutionChain().then(() => evolutionChains());
 }
 
 async function fetchPokemon() {
     try {
-        for (let pokemonIndex = 1; pokemonIndex <= 1; pokemonIndex++) {
-            const url = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`;
-            const response = await fetch(url);
+        for (let pokemonIndex = 1; pokemonIndex <= 10; pokemonIndex++) {
+            let url = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`;
+            let response = await fetch(url);
             if (!response.ok) {
                 throw new Error(`Response status: ${response.status}`);
             }
             const json = await response.json();
-            console.log("json: ", json);
             fetchPokemons.push({
+                id: json.id,
                 name: json.name,
                 image: json.sprites.front_default,
                 types: json.types.map((type) => type.type.name),
             });
+            console.log("Pokemon: ", json);
         }
     } catch (error) {
         console.error(error.message);
@@ -27,35 +31,48 @@ async function fetchPokemon() {
 
 function pokemonCard() {
     let contentContainer = document.getElementById("pokedex");
-    for (let pokemon of fetchPokemons) /*html*/ {
-        contentContainer.innerHTML += `
-            <div class="">
-                <img src="${pokemon.image}" class="" alt="${pokemon.image}">
+    for (let pokemon of fetchPokemons) {
+        contentContainer.innerHTML += /*html*/ `
+            <div class="card d-flex mb-3" style="width: 18rem;"> 
+                <div class="text-end text-xl-end fs-2">#${pokemon.id}</div>
+                <img src="${pokemon.image}" class="card-img-top" alt="${pokemon.image}">
+                <div class="card-body">
                     <div class="">
-                      <h5 class="">${pokemon.name}</h5>
-                      <p class="">${pokemon.types.join(", ")}</p>
+                      <h2 class="card-title">${pokemon.name}</h2>
+                      <p class="card-tex">${pokemon.types.join(", ")}</p>
                       <div>abilities: </div>
                     </div>
+                </div>
             </div>`;
     }
 }
 
-// async function fetchmed() {
-//     try {
-//         for (let pokemonIndex = 1; pokemonIndex <= 2; pokemonIndex++) {
-//             const url = ``;
-//             const response = await fetch(url);
-//             if (!response.ok) {
-//                 throw new Error(`Response status: ${response.status}`);
-//             }
-//             const json = await response.json();
-//             console.log("json: ", json);
-//             fetchPokemons.push({
-//                 name: json.name,
-//             });
-//         }
-//         consol.log("json: ", json)
-//     } catch (error) {
-//         console.error(error.message);
-//     }
-// }
+async function evolutionChain() {
+    try {
+        for (let evolutionIndex = 1; evolutionIndex <=10; evolutionIndex++) {
+            let url = `https://pokeapi.co/api/v2/evolution-chain/${evolutionIndex}`;
+            let response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            const json = await response.json();
+            evolutionChains.push({
+                id: json.chain.id,
+            })
+            console.log("evolutionChain: ", json)
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+function evolutionChainCard() {
+    let contentContainer = document.getElementById("evolutionChain");
+    for (let evolution of evolutionChains) {
+        contentContainer.innerHTML += /*html*/`
+            <div class="card d-flex mb-3" style="width: 18rem;">
+                <div class="text-end text-xl-end fs-2">#${evolution.id}</div>
+            </div>
+        `
+    }
+}
