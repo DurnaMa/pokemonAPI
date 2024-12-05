0c43ab34ea8ec4919c8ac843daeb401125d5233f
Mahir Durna <159443825+DurnaMa@users.noreply.github.com>
2024-12-05 09:54:58 +0100

    New project started

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function init() {
 
 async function fetchPokemon() {
     try {
-        for (let pokemonIndex = 1; pokemonIndex <= 2; pokemonIndex++) {
+        for (let pokemonIndex = 1; pokemonIndex <= 1; pokemonIndex++) {
             const url = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`;
             const response = await fetch(url);
             if (!response.ok) {
@@ -20,7 +20,6 @@ async function fetchPokemon() {
                 types: json.types.map((type) => type.type.name),
             });
         }
-        consol.log("json: ", json) 
     } catch (error) {
         console.error(error.message);
     }
@@ -28,15 +27,35 @@ async function fetchPokemon() {
 
 function pokemonCard() {
     let contentContainer = document.getElementById("pokedex");
-    for (let pokemon of fetchPokemons) {
+    for (let pokemon of fetchPokemons) /*html*/ {
         contentContainer.innerHTML += `
-            <div class="card mb-3 border-4 border-dark" style="width: 18rem;">
-                <img src="${pokemon.image}" class="card-img-top" alt="src="${pokemon.image}"">
-                    <div class="card-body">
-                      <h5 class="card-title">${pokemon.name}</h5>
-                      <p class="card-text">${pokemon.types.join(", ")}</p>
+            <div class="">
+                <img src="${pokemon.image}" class="" alt="${pokemon.image}">
+                    <div class="">
+                      <h5 class="">${pokemon.name}</h5>
+                      <p class="">${pokemon.types.join(", ")}</p>
                       <div>abilities: </div>
                     </div>
             </div>`;
     }
 }
+
+// async function fetchmed() {
+//     try {
+//         for (let pokemonIndex = 1; pokemonIndex <= 2; pokemonIndex++) {
+//             const url = ``;
+//             const response = await fetch(url);
+//             if (!response.ok) {
+//                 throw new Error(`Response status: ${response.status}`);
+//             }
+//             const json = await response.json();
+//             console.log("json: ", json);
+//             fetchPokemons.push({
+//                 name: json.name,
+//             });
+//         }
+//         consol.log("json: ", json)
+//     } catch (error) {
+//         console.error(error.message);
+//     }
+// }
