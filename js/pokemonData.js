be4516c3ebef4d351fc83eeda9ceb8fae7881641
Mahir Durna <159443825+DurnaMa@users.noreply.github.com>
2024-12-10 17:28:42 +0100

    Code optimization

--- js/pokemonData.js
+++ js/pokemonData.js
@@ -1,44 +1,62 @@
-let fetchPokemons = [];
+//let fetchPokemons = [];
 let evolutionChains = [];
+let allPokemon = [];
+let countdown = 25;
+let id = 1;
+let offset = 0;
 
-console.log("Pokemon: ", fetchPokemons);
 async function fetchPokemon() {
     try {
-        for (let pokemonIndex = 1; pokemonIndex <= 5; pokemonIndex++) {
-            let url = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`;
-            let response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-            }
-            const json = await response.json();
-            fetchPokemons.push({
-                id: json.id,
-                name: json.name,
-                image: json.sprites.front_default,
-                types: json.types.map((type) => type.type.name),
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=25&offset=${offset}`);
+        let responseAsJson = await response.json();
+        console.log("response json: ", responseAsJson);
+        for (let pokeIndex = 0;pokeIndex < responseAsJson.results.length;pokeIndex++) {
+            let pokemon = responseAsJson["results"][pokeIndex];
+            console.log(pokemon, " pokemon url: ", pokemon["url"]);
+            let fetchedPokemon = await fetch(`${pokemon["url"]}`);
+            let singlePokemon = await fetchedPokemon.json();
+            //allPokemon.push(singlePokemon);
+
+            allPokemon.push({
+                id: singlePokemon.id,
+                name: singlePokemon.name,
+                image: singlePokemon.sprites.front_default,
+                types: singlePokemon.types.map((type) => type.type.name),
             });
-            console.log("Pokemon: ", json);
         }
+        offset = offset + 25;
+        console.log("all pokemon: ", allPokemon);
     } catch (error) {
         console.error(error.message);
     }
+    pokemonCard();
 }
 
-async function evolutionChain() {
-    try {
-        for (let evolutionIndex = 1; evolutionIndex <= 5; evolutionIndex++) {
-            let url = `https://pokeapi.co/api/v2/evolution-chain/${evolutionIndex}`;
-            let response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-            }
-            const json = await response.json();
-            evolutionChains.push({
-                id: json.chain.id,
-            })
-            console.log("evolutionChain: ", json)
-        }
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+// function pushPokemon() {
+//     const json = allPokemon;
+//     allPokemon.push({
+//         id: singlePokemon.id,
+//         name: singlePokemon.name,
+//         image: singlePokemon.sprites.front_default,
+//         types: singlePokemon.types.map((type) => type.type.name),
+//     });
+// }
+
+// async function evolutionChain() {
+//     try {
+//         for (let evolutionIndex = 1; evolutionIndex <= 5; evolutionIndex++) {
+//             let url = `https://pokeapi.co/api/v2/evolution-chain/${evolutionIndex}`;
+//             let response = await fetch(url);
+//             if (!response.ok) {
+//                 throw new Error(`Response status: ${response.status}`);
+//             }
+//             const json = await response.json();
+//             evolutionChains.push({
+//                 id: json.chain.id,
+//             });
+//             console.log("evolutionChain: ", json);
+//         }
+//     } catch (error) {
+//         console.error(error.message);
+//     }
+// }
--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 function init() {
-    fetchPokemon().then(() => pokemonCard());
-    evolutionChain();
-    //evolutionChain().then(() => evolutionChainCard());
+    //evolutionChain();
+    fetchPokemon();
+    pokemonCard();
 }
 
 function pokemonCard() {
     let contentContainer = document.getElementById("pokedex");
-    for (let pokemonIndex = 0; pokemonIndex < fetchPokemons.length; pokemonIndex++) {
-        let pokemon = fetchPokemons[pokemonIndex];
+    for (
+        let pokemonIndex = 0;
+        pokemonIndex < allPokemon.length;
+        pokemonIndex++
+    ) {
+        let singlePokemon = allPokemon[pokemonIndex];
         contentContainer.innerHTML += /*html*/ `
-        <div onclick="openPokemonCard(${pokemonIndex})" class="mahir-card">
-                <img class="object-cover" src="${pokemon.image}" alt="${pokemon.name}">
-                <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h2 class="text-gray-900">#${pokemon.id}</h2>
-                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${pokemon.name}</h2>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${pokemon.types.join(", ")}</p>
-                </div>
-        </div>`;
+            <div onclick="openPokemonCard(${pokemonIndex})" class="mahir-card">
+                    <img class="object-cover" src="${singlePokemon.image}" alt="${singlePokemon.name}">
+                    <div class="flex flex-col justify-between p-4 leading-normal">
+                        <h2 class="text-white ">#${singlePokemon.id}</h2>
+                        <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${
+                            singlePokemon.name
+                        }</h2>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${singlePokemon.types.join(
+                            ", "
+                        )}</p>
+                    </div>
+            </div>`;
     }
 }
 
@@ -38,34 +46,60 @@ function evolutionChainCard() {
 }
 
 function openPokemonCard(pokemonIndex) {
-    
-        let pokemon = fetchPokemons[pokemonIndex];
-        document.getElementById("pokedexBigCard").classList.remove("d-none");
-        document.getElementById("pokedexBigCard").innerHTML = /*html*/ `
-        <div class="flex items-center justify-center"> 
-            <img src="./assets/icons/back.png" alt="back" class=" w-12 h-auto rounded-s-3xl ">     
-            <div class="flex-auto  max-w-sm bg-gray-800 border border-cyan-950 rounded-lg">
-                <img onclick="closePokemonCard()" class="bg-white border cursor-pointer w-8 h-8 items-end justify-end flex" src="./assets/icons/close.png">
-                <div class="flex justify-center items-center border-b-8 border-black ">
-                    <img class="rounded-t-lg object-cover" src="${pokemon.image}" alt="${pokemon.name}" />
+    let pokemon = fetchPokemons[pokemonIndex];
+    document.getElementById("pokedexBigCard").classList.remove("d-none");
+    document.getElementById("pokedexBigCard").innerHTML = /*html*/ `
+            <div class="flex items-center justify-center">
+                <button onclick=nextCardR()> 
+                    <img src="./assets/icons/back.png" alt="back" class=" w-12 h-auto rounded-s-3xl ">
+                </button>     
+                <div class="flex-auto  max-w-sm bg-gray-800 border border-cyan-950 rounded-lg">
+                    <img onclick="closePokemonCard()" class="bg-white border cursor-pointer w-8 h-8 items-end justify-end flex" src="./assets/icons/close.png">
+                    <div class="flex justify-center items-center border-b-8 border-black ">
+                        <img class="rounded-t-lg object-cover" src="${pokemon.image}" alt="${pokemon.name}" />
+                    </div>
+                    <div class="p-5">
+                        <div style="width: 60%; ">
+                            <canvas id="myChart" width="400" height="400"></canvas>
+                        </div>
+                        <!-- <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a> -->
+                    </div>
                 </div>
-                <div class="p-5">
-                    <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Read more
-                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                    </a>
-                </div>
-            </div>
-            <img src="./assets/icons/next.png" alt="next" class="w-12 h-auto border-solid border-stone-950 ">
-        </div>`;
+                <button onclick="nextCardL()">
+                    <img src="./assets/icons/next.png" alt="next" class="w-12 h-auto border-solid border-stone-950 ">
+                </button>
+            </div>`;
 }
 
 function closePokemonCard() {
     document.getElementById("pokedexBigCard").classList.add("d-none");
+}
+
+function nextCardL(pokemonIndex) {
+    if (pokemonIndex > 0) {
+        pokemonIndex -= 1;
+    } else {
+        pokemonIndex = fetchPokemons.length -1;
+    }
+
+    openPokemonCard(pokemonIndex);
+}
+
+function nextCardR(pokemonIndex) {
+    if (pokemonIndex <  fetchPokemons.length -1) {
+        pokemonIndex += 1;
+    } else {
+        pokemonIndex = 0;
+    }
+
+    openPokemonCard(pokemonIndex);
 }
